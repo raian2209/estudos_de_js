13a9174f20bf1237c094f366e60a04a870547773
Raimundo Andrade de Lima Neto <andradeneto1999@gmail.com>
2022-01-08 07:52:28 -0300

    .
    result final

--- aula12.ex/script.js
+++ aula12.ex/script.js
@@ -3,16 +3,19 @@ function carregar(){
     var img = window.document.getElementById(`imagem`)
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 20 teste forçado 
+    //var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
         //Bom Dia!
         img.src = 'fotomanha.png'
+        document.body.style.background ='#ffb868'
     } else if (hora >= 12 && hora < 18){
         // Boa Tarde!
         img.src = 'fototarde.png'
+        document.body.style.background = '#c78e76'
     } else {
         // Boa Noite!
         img.src = 'fotonoite.png'
+        document.body.style.background = '#582c40'
     }
 }
